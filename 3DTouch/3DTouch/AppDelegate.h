c481359b805f5f034cf96682a36d1d6825d6a16d
zhangrong <zhangrong@wurongdeMacBook-Pro.local>
2017-04-25 16:01:08 +0800

    init project

---
+++ 3DTouch/3DTouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3DTouch
+//
+//  Created by zhangrong on 2017/4/25.
+//  Copyright © 2017年 ryan.zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 3DTouch/3DTouch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3DTouch
+//
+//  Created by zhangrong on 2017/4/25.
+//  Copyright © 2017年 ryan.zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
